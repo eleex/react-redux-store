45a465efe59c9cc1dfdbcb15014351f8eb56db65
eleex <eleex1988@gmail.com>
2020-04-19 02:03:18 +0300

    Updates

---
+++ precache-manifest.a879e6135fe6c2fa17d69a70d4c89860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7907b72e6b02c64b02c8ad4ffa9743d7",
+    "url": "/react-redux-store/index.html"
+  },
+  {
+    "revision": "879a46bab8cbed843666",
+    "url": "/react-redux-store/static/css/main.7ca19a4c.chunk.css"
+  },
+  {
+    "revision": "1590e173f4e7fd6a68e6",
+    "url": "/react-redux-store/static/js/2.25857173.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-store/static/js/2.25857173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879a46bab8cbed843666",
+    "url": "/react-redux-store/static/js/main.52bcb0d9.chunk.js"
+  },
+  {
+    "revision": "923fc0eb174bd3e07e80",
+    "url": "/react-redux-store/static/js/runtime-main.f5d70cde.js"
+  }
+]);
